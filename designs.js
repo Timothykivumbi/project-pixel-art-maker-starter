65a88eee3f271f51aa1b56aa8843da7e1b8f2f94
Timothykivumbi <40731586+Timothykivumbi@users.noreply.github.com>
2018-07-01 11:03:43 +0300

    designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,57 @@
-// Select color input
+// Select colour input
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$(document).ready(function () {
 
-// Your code goes here!
+ $("#sizePicker").submit(function (event) {
+
+   event.preventDefault();
+
+
+
+   var row;
+
+   var column;
+
+   var color;
+
+
+
+   row = $("#inputHeight").val();   // height
+
+   column = $("#inputWidth").val();   // width
+
+   makeGrid(row, column);
+
+ });
+
+});
+
+
+
+function makeGrid(height, width) {
+
+ $("tr").remove();
+
+
+
+ // Your code goes here!
+
+ for (var x = 1; x <= height; x++) {
+
+   $("#pixelCanvas").append("<tr></tr>");
+
+
+
+   for (var y = 1; y <= width; y++) {
+
+     $('tr').filter(':last').append('<td></td>');
+
+   }
+
+ }
 
 }
